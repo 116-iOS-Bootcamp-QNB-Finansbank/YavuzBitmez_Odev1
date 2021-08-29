ea9ab24b92385428f90144d34caed22ac9171ec6
Yavuz Selim Bitmez <ybitmez@nettsi.com>
2021-08-29 10:21:46 +0300

    Project created.

---
+++ patika_Bootcamp_Odev_1_hafta/patika_Bootcamp_Odev_1_hafta/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  patika_Bootcamp_Odev_1_hafta
+//
+//  Created by Yavuz Selim Bitmez on 29.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
